66e9b5902f3962e91ca5983bab1c131e84746bd8
ljw1412 <506644791@qq.com>
2019-06-02 21:19:00 +0800

    update GlideView.

---
+++ app/src/main/java/com/acg/manzhai/views/GlideView.kt
@@ -0,0 +1,42 @@
+package com.acg.manzhai.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.RequestManager
+
+class GlideView
+
+@JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
+    private val TAG: String = "GlideView"
+    private lateinit var mGlide: RequestManager
+
+    init {
+        initGlide()
+    }
+
+
+    override fun onDetachedFromWindow() {
+        clearGlide()
+        super.onDetachedFromWindow()
+    }
+
+
+    fun initGlide(){
+        mGlide = Glide.with(context)
+    }
+
+    fun loadImage(url: String){
+        Log.d(TAG,"loadImageUrl: $url, context: ${context.javaClass}")
+        mGlide.asBitmap().load(url).into(this)
+    }
+
+    fun clearGlide(){
+        mGlide.clear(this)
+    }
+}
